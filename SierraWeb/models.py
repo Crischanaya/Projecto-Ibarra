11b9866990c8b4df35cf86bb992eee5e216a9fce
Noelt21 <71086161+Noelt21@users.noreply.github.com>
2021-01-02 20:12:29 -0700

    admin

--- SierraWeb/models.py
+++ SierraWeb/models.py
@@ -9,7 +9,7 @@ class Usuarios(models.Model):
     password = models.CharField(max_length=20)
     
     def nombreCompleto(self):
-        txt = "{0} {1}/ Correo: {2}"
+        txt = "{0} {1} / Correo: {2}"
         return txt.format(self.nombre_usuario,self.apellido,self.correo)
 
     def __str__(self):
@@ -22,7 +22,7 @@ class Paquetes(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=0)
     
     def nombrePaquetes(self):
-        txt = "{0}/ Lugar: {1} / Precio: {2} "
+        txt = "{0}  /  Lugar: {1}  /  Precio: {2} "
         return txt.format(self.nombre,self.lugar,self.precio)
 
     def __str__(self):
@@ -34,6 +34,6 @@ class UserPaquete(models.Model):
     id_usuario = models.ForeignKey(Usuarios ,on_delete=models.CASCADE)
 
     def __str__(self):
-        txt = " Usuario: {0} / Paquete: {1}"
+        txt = " Usuario: {0}  /  Paquete: {1}"
         return txt.format(self.id_usuario.nombreCompleto(),self.id_paquete.nombrePaquetes())
         
