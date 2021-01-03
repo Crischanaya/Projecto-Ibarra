857ea873f57590bd9c80008e3beb09ea93c0eeaf
Noelt21 <71086161+Noelt21@users.noreply.github.com>
2021-01-02 21:07:50 -0700

    CONTACTO

--- SierraWeb/views.py
+++ SierraWeb/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def barrancas(request):
     return render(request, "SierraWeb/barrancas.html")
 
 def contacto(request):
+    if request.method=="POST":
+       
+        subject=request.POST["asunto"] 
+
+        if request.POST["contacto"] == "telefono":
+            contactar = "telefono"
+        else:
+            contactar = "correo"      
+
+        message= "Nombre: "+ request.POST["nombre"]  +"\nMensaje:"+ request.POST["mensaje"] + "\nCorreo: " + request.POST["correo"] + "\nTeléfono: " + request.POST["telefono"] + "\nContactar: " + contactar + "\nFecha a Contactar: " + request.POST["fecha"] + "\nHora a Contactar: " + request.POST["hora"]
+
+        email_from=settings.EMAIL_HOST_USER
+
+        recipient_list=["agenciasierratours@gmail.com"]
+
+        send_mail(subject, message, email_from, recipient_list)
+
     return render(request, "SierraWeb/contacto.html")
 
 def creel(request):
